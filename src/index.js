01901b453c69682ad3b7c24be9dd655ef3a34cca
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-12-12 13:04:45 -0300

    commit init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/global.css';
+import PageRouter from './pages/Router';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PageRouter />
   </React.StrictMode>
 );
 
