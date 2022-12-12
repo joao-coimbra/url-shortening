0cd86ac52a7187c8f12b5c2346e7d2ce2bd35736
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-12-12 13:06:47 -0300

    API

---
+++ src/services/api/url.api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/short/url";
+
+export const api = {
+    create(originURL, shortId) {
+        return axios.post(API_URL, {url: originURL, UD: shortId})
+    },
+	getOne(id) {
+		return axios.get(API_URL + "/" + id);
+	},
+	getAll() {
+		return axios.get(API_URL);
+	},
+    getQuery(query) { return axios.get(API_URL + '/findby' + query) },
+	getAllByIndustry(industryId) { return axios.get(API_URL + '/industry/' + industryId) },
+};
