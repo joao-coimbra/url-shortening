695d6668a4c6c0a5ccb483a47af0577277753989
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-12-13 13:18:20 -0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	// darkMode: 'class',
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"primary-color": "#45CDE9",
+				"secondary-color": "#7A8FD3",
+			},
+		},
+	},
+	plugins: [],
+};
